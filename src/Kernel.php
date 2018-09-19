da87d7e2550d1ac561e5e5f9271280a32ef1c9bc
Alexander Schranz <alexander@sulu.io>
2018-09-19 16:51:43 +0200

    Use symfony 4 folder structure and remove preconfigured configurations (#98)
    * change folder structure to symfony 4
    
    * fix maintenance mode for symfony 4 and remove massive search config
    
    * fix loading of routing
    
    * move set of kernel.var_dir to sulu repo
    
    * remove prepended configuration for structure paths
    
    * remove preconfigured symfony cmf configuration
    
    * move twig templates into pages folder
    
    * use fork for running tests on sulu sulu pr
    
    * restructure after review
    
    * fixed env and rnemae security admin
    
    * remove unneded .gitkeep
    
    * remove translation bundle
    
    * build js and css
    
    * add new build
    
    * fix js build
    
    * remove symfony flex because of no compatibility
    
    * add required env variables for travis
    
    * fix bin and public folder in composer scripts
    
    * set composer bin directory
    
    * remove symfony flex
    
    * code style fixes
    
    * remove doctrine database version and let doctrine detect the version
    
    * remove repositories flag
    
    * fixed composer autoscripts
    
    * fix .env comment and travis script
    
    * fix linting tasks
    
    * remove prefered install from composer
    
    * add appveyor to sulu minimal tests
    
    * try to fix appveyor env variables
    
    * comment database connection because cache:clear command is not working with it

---
+++ src/Kernel.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+use Sulu\Component\HttpKernel\SuluKernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class Kernel extends SuluKernel
+{
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader)
+    {
+        // Feel free to remove the "container.autowiring.strict_mode" parameter
+        // if you are using symfony/dependency-injection 4.0+ as it's the default behavior
+        $container->setParameter('container.autowiring.strict_mode', true);
+        $container->setParameter('container.dumper.inline_class_loader', true);
+
+        parent::configureContainer($container, $loader);
+    }
+}
