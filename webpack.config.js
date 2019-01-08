181eed6af1545d5de9fa34bd3d159512a5b2e479
Alexander Schranz <alexander@sulu.io>
2019-01-08 13:28:15 +0100

    Fix accidently cleanup of build folder by IDE (#151)
    * Fix accidently cleanup of build folder by IDE
    
    * fix ide problems with reading of the webpack config
    
    * Fix ide problems with reading of the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
 const webpackConfig = require('./vendor/sulu/sulu/webpack.config.js');
 
-module.exports = webpackConfig;
+module.exports = (env, argv) => {
+    env = env ? env : {};
+    argv = argv ? argv : {};
+
+    env.root_path = __dirname;
+
+    const config = webpackConfig(env, argv);
+
+    // For adding custom admin js file uncomment the following line:
+    // config.entry.unshift(path.resolve(__dirname, 'assets/admin/index.js'));
+
+    return config;
+};
