4e1ebe683fc2600008e932532ace5a9ff6a3ff3a
Daniel Rotter <daniel.rotter@gmail.com>
2019-09-18 10:55:40 +0200

    Adjust to integration of CollaborationBundle into AdminBundle (#199)
    * Adjust to integration of CollaborationBundle into AdminBundle
    
    * Add new build

--- config/bundles.php
+++ config/bundles.php
@@ -41,7 +41,6 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true, 'admin' => true],
     Sulu\Bundle\AdminBundle\SuluAdminBundle::class => ['all' => true, 'admin' => true],
-    Sulu\Bundle\CollaborationBundle\SuluCollaborationBundle::class => ['all' => true, 'admin' => true],
     Sulu\Bundle\PreviewBundle\SuluPreviewBundle::class => ['all' => true, 'admin' => true],
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true, 'admin' => true],
     Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle::class => ['all' => true, 'website' => true],
