840cf3ca1a07e937a5a677f2fd747cf66fddc005
Daniel Rotter <daniel.rotter@gmail.com>
2019-09-23 17:02:49 +0200

    Change header comments

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,7 +5,7 @@ namespace App;
 /*
  * This file is part of Sulu.
  *
- * (c) MASSIVE ART WebServices GmbH
+ * (c) Sulu GmbH
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
