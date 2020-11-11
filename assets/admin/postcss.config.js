26ccc99ce7fbef0224a2c5bf285688824316cf2a
nnatter <niklas.natter@gmail.com>
2020-11-11 11:48:51 +0100

    Copy content of postcss.config.js from Sulu

--- assets/admin/postcss.config.js
+++ assets/admin/postcss.config.js
@@ -1,2 +1,14 @@
-/* reuse the postcss configuration included in sulu */
-module.exports = require('../../vendor/sulu/sulu/postcss.config.js');
+const path = require('path');
+
+module.exports = {
+    plugins: {
+        'postcss-import': {
+            path: path.resolve(process.cwd(), 'node_modules'),
+        },
+        'postcss-nested': {},
+        'postcss-simple-vars': {},
+        'postcss-calc': {},
+        'postcss-hexrgba': {},
+        'autoprefixer': {},
+    },
+};
