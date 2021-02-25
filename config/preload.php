6870711157da0fa897a8cc594ad6f375cbc444b9
Alexander Schranz <alexander@sulu.io>
2021-02-25 18:26:00 +0100

    Bump Version

---
+++ config/preload.php
@@ -0,0 +1,9 @@
+<?php
+
+// if (file_exists(dirname(__DIR__).'/var/cache/admin/prod/App_KernelProdContainer.preload.php')) {
+//    require dirname(__DIR__).'/var/cache/admin/prod/App_KernelProdContainer.preload.php';
+// }
+
+// if (file_exists(dirname(__DIR__).'/var/cache/website/prod/App_KernelProdContainer.preload.php')) {
+//     require dirname(__DIR__).'/var/cache/website/prod/App_KernelProdContainer.preload.php';
+// }
