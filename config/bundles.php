71de931dbf90c37f184f3fe31d917c52501a5b96
nnatter <niklas@sulu.io>
2021-09-06 14:46:08 +0200

    Register and enable SuluTrashBundle (#136)
    * Register and enable SuluTrashBundle
    
    * Build administration interface assets

--- config/bundles.php
+++ config/bundles.php
@@ -43,4 +43,5 @@ return [
     Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle::class => ['all' => true, 'website' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Sulu\Bundle\ActivityBundle\SuluActivityBundle::class => ['all' => true],
+    Sulu\Bundle\TrashBundle\SuluTrashBundle::class => ['all' => true],
 ];
