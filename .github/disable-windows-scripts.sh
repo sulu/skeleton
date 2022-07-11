7df4225e05ab5cc93fc7033b030d27d763cbf1a6
Alexander Schranz <alexander@sulu.io>
2022-07-11 23:08:22 +0200

    Update script

--- .github/disable-windows-scripts.sh
+++ .github/disable-windows-scripts.sh
@@ -2,3 +2,5 @@
 
 php -r 'file_put_contents("composer.json", str_replace("\"lint-php-cs", "\"deactivate-lint-php-cs-fix", file_get_contents("composer.json")));'
 php -r 'file_put_contents("composer.json", str_replace("\"lint-rector", "\"deactivate-lint-rector", file_get_contents("composer.json")));'
+
+cat composer.json
