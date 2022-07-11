174aebbd8f4daef2d7db74110cc12b66131019cb
Alexander Schranz <alexander@sulu.io>
2022-07-11 23:17:32 +0200

    Use gitlab ci script

--- .github/disable-windows-scripts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-php -r 'file_put_contents("composer.json", str_replace("\"lint-php-cs", "\"deactivate-lint-php-cs-fix", file_get_contents("composer.json")));'
-php -r 'file_put_contents("composer.json", str_replace("\"lint-rector", "\"deactivate-lint-rector", file_get_contents("composer.json")));'
-
-cat composer.json
