33e094355b66755ba6e8d2a623feb9be3fa7c75e
Alexander Schranz <alexander@sulu.io>
2022-07-23 21:15:34 +0200

    Use stricter comparison

--- config/router.php
+++ config/router.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
 // Workaround https://bugs.php.net/64566
 $autoPrependFile = \ini_get('auto_prepend_file');
 
-if (!empty($autoPrependFile) && !\in_array(\realpath($autoPrependFile), \get_included_files(), true)) {
+if (false !== (bool) $autoPrependFile && !\in_array(\realpath($autoPrependFile), \get_included_files(), true)) {
     require \ini_get('auto_prepend_file');
 }
 
