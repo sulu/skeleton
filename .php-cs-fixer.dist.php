ce45adcdd7f8694f2552690c3a0aa06c6f136217
Alexander Schranz <alexander@sulu.io>
2022-08-03 13:34:45 +0200

    Ignore all php files from .gitignore for php-cs-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
-    ->exclude(['var', 'lib', 'node_modules'])
+    ->ignoreVCSIgnored(true)
     ->notName('bundles.php');
 
 return (new PhpCsFixer\Config())
