a3eb95acb654bd5c95f2bfc21cc3b3b3a9af5a56
Alexander Schranz <alexander@sulu.io>
2024-03-21 17:15:52 +0100

    Move from dantleech/phpcr-migrations-bundle to the phpcr/phpcr-migrations-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -31,7 +31,7 @@ return [
     Sulu\Bundle\CustomUrlBundle\SuluCustomUrlBundle::class => ['all' => true],
     Sulu\Bundle\RouteBundle\SuluRouteBundle::class => ['all' => true],
     Sulu\Bundle\MarkupBundle\SuluMarkupBundle::class => ['all' => true],
-    DTL\Bundle\PhpcrMigrations\PhpcrMigrationsBundle::class => ['all' => true],
+    PHPCR\PhpcrMigrationsBundle\PhpcrMigrationsBundle::class => ['all' => true],
     Massive\Bundle\BuildBundle\MassiveBuildBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Sulu\Bundle\TestBundle\SuluTestBundle::class => ['dev' => true, 'test' => true],
