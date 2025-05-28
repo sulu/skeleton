385c944aae26742d90edfc6f7bafe85ca8fae720
Alexander Schranz <alexander@sulu.io>
2025-05-28 17:06:36 +0200

    Upgrade to PHPUnit 11

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,3 +9,7 @@ require \dirname(__DIR__) . '/vendor/autoload.php';
 if (\method_exists(Dotenv::class, 'bootEnv')) { // @phpstan-ignore-line function.alreadyNarrowedType
     (new Dotenv())->bootEnv(\dirname(__DIR__) . '/.env');
 }
+
+if ($_SERVER['APP_DEBUG']) {
+    \umask(0000);
+}
